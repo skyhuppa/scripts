e46405933763b2fd3292474b7664dfef56a4350b
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2022-07-18 13:29:56 -0700

    Update arch-setup.sh

--- personal-setup/arch-setup.sh
+++ personal-setup/arch-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-git config --global user.name "Akhil Narang"
-git config --global user.email "akhilnarang.1999@gmail.com"
+git config --global user.name "skyhuppa"
+git config --global user.email "skyhuppa@hotmail.co.uk"
 git config --global credential.helper "cache --timeout=7200"
 printf '\n' | tee -a ~/.bashrc
 echo "source ~/scripts/functions" >> ~/.bashrc
