3299a036427d7629974a55e5d75076ccec9daa6a
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2022-07-24 13:00:10 -0700

    Update deb-setup.sh

--- personal-setup/deb-setup.sh
+++ personal-setup/deb-setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 source ~/scripts/setup/hub.sh
-git config --global user.name "Akhil Narang"
-git config --global user.email "akhilnarang.1999@gmail.com"
+git config --global user.name "adasi kaakyire"
+git config --global user.email "skyhuppa@hotmail.co.uk"
 git config --global credential.helper "cache --timeout=7200"
 printf '\n' | tee -a ~/.bashrc
 echo "source ~/scripts/functions" >> ~/.bashrc
