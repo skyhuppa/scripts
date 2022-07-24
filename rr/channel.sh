9f92144eba5c026ca66caeba23a29702807f01eb
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2022-07-24 13:08:18 -0700

    Update channel.sh

--- rr/channel.sh
+++ rr/channel.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 [[ -z ${API_KEY} ]] && echo "API_KEY not defined, exiting!" && exit 1
-CHAT_ID="-1001185331716"
+CHAT_ID="-922861774"
 curl -s "https://api.telegram.org/bot${API_KEY}/sendmessage" --data "text=$BUILD_URL&chat_id=$CHAT_ID&parse_mode=HTML"
 CHAT_ID="@ResurrectionRemixChannel"
 for device in $(git diff HEAD@\{1\}..HEAD --name-only); do
     d=${device/.json/}
-    CHAT_ID="-1001185331716"
+    CHAT_ID="-922861774"
     authors="$(git log HEAD@\{1\}..HEAD --pretty=format:"%an")"
     newhash="$(git rev-parse HEAD)"
     oldhash="$(git rev-parse HEAD@\{1\})"
